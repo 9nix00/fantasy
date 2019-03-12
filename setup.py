dbd57f77c578c6f913c73b8faf7736ce6552659c
Stormxx <stormxx@1024.engineer>
2019-03-12 17:14:57 +0800

    try use new format to create fantasy

--- setup.py
+++ setup.py
@@ -6,13 +6,18 @@ version = "0.2.12"
 from os import path
 
 this_directory = path.abspath(path.dirname(__file__))
-with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+with open(path.join(this_directory, 'README.md')) as f:
     long_description = f.read()
+
+with open(path.join(this_directory, 'requirements.txt')) as f:
+    requirements_list = [v.strip() for v in f.readlines() if
+                         v.strip() and v.strip()[0] != '#']
 
 setup(
     name='flask-fantasy',
     long_description=long_description,
     long_description_content_type='text/markdown',
+    install_requires=requirements_list,
     version=version,
     packages=find_packages(exclude=["tests"]),
     url='https://github.com/wangwenpei/fantasy',
