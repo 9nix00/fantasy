ead4b8be138822c2178a1b50922d2183a8d01603
Stormxx <stormxx@1024.engineer>
2019-03-12 15:16:53 +0800

    use markdown as default description format

--- setup.py
+++ setup.py
@@ -2,8 +2,17 @@ from setuptools import find_packages, setup
 
 version = "0.2.12"
 
+# read the contents of your README file
+from os import path
+
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
 setup(
     name='flask-fantasy',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
     version=version,
     packages=find_packages(exclude=["tests"]),
     url='https://github.com/wangwenpei/fantasy',
