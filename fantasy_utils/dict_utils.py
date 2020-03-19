df300afeea326de5add740324d09dc9978aecd35
stormxx <stormxx@1024.engineer>
2020-03-20 00:12:14 +0800

    improve dict

---
+++ fantasy_utils/dict_utils.py
@@ -0,0 +1,34 @@
+"""
+=====================
+improve builtin-dict
+=====================
+"""
+
+import json
+
+
+class objdict(dict):
+    def __getattr__(self, name):
+        if name in self:
+            return self[name]
+        else:
+            raise AttributeError("No such attribute: " + name)
+
+    def __setattr__(self, name, value):
+        self[name] = value
+
+    def __delattr__(self, name):
+        if name in self:
+            del self[name]
+        else:
+            raise AttributeError("No such attribute: " + name)
+
+
+class objectview(object):
+    def __init__(self, d):
+        self.__dict__ = d
+
+
+class jsonview(object):
+    def __init__(self, d):
+        self.__dict__ = json.loads(d)
